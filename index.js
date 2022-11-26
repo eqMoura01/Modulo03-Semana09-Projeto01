b520d7c42c34b409319ef8b55c0d58ef1bcda6ea
Victor Mora <victorlvmora@gmail.com>
2022-11-26 13:32:01 -0300

    Correções
    Correção e implementação de novos metodos

--- index.js
+++ index.js
@@ -1,61 +1,147 @@
-const lista = document.getElementById('listaCompras');
+// Variaveis globais
 
-let htmlSaved = document.getElementsByTagName('html');
+// Responsavel por controlar o input de produto
+const produtoInput = document.getElementById('produtoInpt');
 
-const inpt = document.getElementById('produtoInpt');
-
-const inptBtn = document.getElementById('inptBtn');
-inptBtn.innerHTML = 'Adicionar';
-
-const valorTotal = document.getElementById('valorTotal');
-
-let total = Number(0);
-
-inptBtn.addEventListener('click', () => {
-    if (inpt.value !== '') {
-        if (inpt.value.length >= 8 && inpt.value.length <= 64) {
-            let produto = document.createElement('li');
-            lista.prepend(produto);
-            produto.innerHTML = inpt.value;
-            produto.setAttribute('style', 'display: flex;align-items: center;gap: 15px;font-size: 25px;height: 40px;');
-            inpt.value = '';
-
-            let checkbox = document.createElement('input');
-            checkbox.setAttribute('type', 'checkbox');
-            checkbox.setAttribute('style', 'height: 25px;width: 25px;');
-            let promptValue = 'a';
-            checkbox.addEventListener('click', () => {
-                if (checkbox.checked) {
-                    promptValue = 'a';
-
-                    while (isNaN(promptValue) == true) {
-                        promptValue = prompt(`Insira o valor do produto ${produto.textContent.toUpperCase()}`);
-                    }
-
-                    total += Number(promptValue);
-
-                    valorTotal.textContent = `R$ ${Number(total)}`;
-
-                    checkbox.disabled = true;
-
-                    produto.setAttribute('style', 'text-decoration: line-through;');
-                }
-
-            });
-
-            produto.prepend(checkbox);
-
-            let img = document.createElement('img');
-            img.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/6/61/Cross_icon_%28white%29.svg/1024px-Cross_icon_%28white%29.svg.png';
-            img.addEventListener('click', () => {
-                lista.removeChild(produto);
-            })
-
-            produto.append(img)
-        }
-        
-        localStorage.setItem('teste', JSON.stringify(htmlSaved));
-
+// Responsavel por controlar o button de produto
+const adicionarButton = document.getElementById('inptBtn');
+adicionarButton.addEventListener('click', () => {
+    let produto = {
+        produtoNome: produtoInput.value,
+        produtoValor: 0,
+        jaTemValor: false
     }
 
-});
+    listaProdutos.push(produto);
+    adicionarProdutoToListaCompras(produto.produtoNome);
+
+})
+
+// Responsavel por controlar a lista de compras
+const listaCompras = document.getElementById('listaCompras');
+
+// Responsavel por controlar o valor do carrinho
+const valorCarrinho = document.getElementById('valorTotal')
+
+// Lista de produtos. Se existir algo no localStorage com a key passada, será isso; Se nao existir nd, é uma lista vazia
+let listaProdutos = JSON.parse(localStorage.getItem('ProdutosSalvos')) || [];
+
+listaProdutos.forEach((element) => {
+    adicionarProdutoToListaCompras(element.produtoNome, element.jaTemValor);
+})
+
+atualizarValorCarrinho();
+
+// Responsavel por adicionar produtos a lista de compras(tanto pelo input, quanto o localStorage)
+function adicionarProdutoToListaCompras(nome, jaTemValor) {
+
+    let produto = {
+        produtoNome: '',
+        produtoValor: 0,
+        jaTemValor: false
+    }
+
+    // Cria um objeto de lista
+    let objetoLista = document.createElement('li');
+
+    // Cria um checkbox para o objeto da lista
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.style.height = '25px';
+    checkbox.style.width = '25px';
+    
+
+    if(jaTemValor == true){
+        checkbox.disabled = true;
+        checkbox.checked = true;
+    }
+    checkbox.addEventListener('click', () => {
+        produto.produtoValor = adicionarValorAoProduto()
+        atualizarValorCarrinho()
+
+        let index = listaProdutos.findIndex((obj) => obj.produtoNome === produto.produtoNome)
+        if (index > -1) {
+            listaProdutos[index].produtoValor = produto.produtoValor;
+
+            salvarListaProdutoLocalStorage(listaProdutos);
+            atualizarValorCarrinho()
+            listaProdutos[index].jaTemValor = true;
+            checkbox.disabled = true;
+        }
+
+        salvarListaProdutoLocalStorage(listaProdutos)
+    })
+
+    // Cria o nome do produto para o objeto da lista
+    let nomeProduto = document.createElement('span');
+    nomeProduto.innerHTML = nome;
+    nomeProduto.style.display = 'flex';
+    nomeProduto.style.alignItems = 'center';
+    nomeProduto.style.gap = '15px';
+    nomeProduto.style.fontSize = '25px';
+    nomeProduto.style.height = '40px'
+
+    produto.produtoNome = nome;
+
+    // Cria o btn para o objeto da lista
+    let removerButton = document.createElement('img');
+    removerButton.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/6/61/Cross_icon_%28white%29.svg/1024px-Cross_icon_%28white%29.svg.png';
+    removerButton.addEventListener('click', () => {
+        listaCompras.removeChild(objetoLista);
+        removerItemDaListaCompras(produto);
+        salvarListaProdutoLocalStorage(listaProdutos)
+    })
+
+    objetoLista.append(checkbox, nomeProduto, removerButton);
+
+    listaCompras.appendChild(objetoLista);
+
+    salvarListaProdutoLocalStorage(listaProdutos);
+
+    return produto;
+}
+
+// Responsavel por salvar o objeto em localStorage
+function salvarListaProdutoLocalStorage(listaProdutos) {
+    localStorage.setItem('ProdutosSalvos', JSON.stringify(listaProdutos))
+}
+
+// Responsavel por obter o objeto do localStorage
+function carregarListaProdutosLocalStorage() {
+    listaProdutos = JSON.parse(localStorage.getItem('ProdutosSalvos'));
+    atualizarValorCarrinho();
+}
+
+// Responsavel por remover itens da lista de compras
+function removerItemDaListaCompras(objetoLista) {
+
+    let index = listaProdutos.findIndex((obj) => obj.produtoNome === objetoLista.produtoNome)
+    if (index > -1) {
+        listaProdutos.splice(index, 1);
+        atualizarValorCarrinho();
+        salvarListaProdutoLocalStorage(listaProdutos);
+    }
+
+}
+
+// Responsavel por atualizar o valor do carrinho
+function atualizarValorCarrinho() {
+    let totalCarrinho = 0;
+
+    listaProdutos.forEach((obj) => {
+        totalCarrinho += Number(obj.produtoValor);
+    })
+
+    valorCarrinho.innerHTML = `R$ ${totalCarrinho.toFixed(2)}`
+}
+
+// Responsavel por adicionar valor ao produto
+function adicionarValorAoProduto() {
+    let valor = 'a';
+
+    while (isNaN(valor) == true) {
+        valor = Number(prompt('Insira o valor do produto'));
+    }
+
+    return valor;
+}
